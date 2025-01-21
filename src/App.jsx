9df9a243e963980f7b70d6a1f4d4c5371660dc0a
PrudhviKleza <prudhvi.macherla@kleza.io>
2025-01-21 14:31:58 +0530

    change in line no.45 div replaced as modal

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+Modal.setAppElement("#root");
 function App() {
   const [count, setCount] = useState(0)
 
@@ -41,7 +42,7 @@ function App() {
         >
           Close
         </button>
-      </div>
+      </Modal>
       
     </>
   )
