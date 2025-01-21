ff8f3f81d76fb0e45bbe1d38af4a27e553ea5ada
PrudhviKleza <prudhvi.macherla@kleza.io>
2025-01-21 12:27:35 +0530

    Revert "Feature3"

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,35 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import Modal from "react-modal";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-Modal.setAppElement("#root");
 function App() {
-  const [count, setCount] = useState(0);
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [count, setCount] = useState(0)
 
-  const increment = () => {
-    alert(`count is ${count}`);
-    setCount(count + 1);
-  };
   return (
     <>
-      <h1>count is {count}</h1>
-      <div className="card">
-        <button onClick={increment}>increment</button>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-      <button onClick={()=>setIsModalOpen(true)}>open modal</button>
-      <Modal
-        isOpen={isModalOpen}
-        onRequestClose={() => setIsModalOpen(false)}
-        style={{
-          content: {
-            width: "300px",
-            margin: "auto",
-            padding: "20px",
-            borderRadius: "10px",
-            textAlign: "center",
-          },
-        }}
-      >
-        <h1 style={{ color:'black'}}>count value is{count}</h1>
-        <button
-          style={{
-            backgroundColor: "#dc3545",
-            color: "red",
-          }}
-          onClick={() => setIsModalOpen(false)}
-        >
-          Close
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
         </button>
-      </Modal>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
