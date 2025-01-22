905217f6f503de5e9413dce4be4f127d4aa79699
PrudhviKleza <prudhvi.macherla@kleza.io>
2025-01-22 10:55:31 +0530

    Revert "implemented feature3"
    This reverts commit e8c1fef53fa10de6db62a338af878b132f259f6e.

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Modal from "react-modal";
 
-Modal.setAppElement("#root");
 function App() {
-  const [count, setCount] = useState(0);
-  const[isModalOpen,setIsModalOpen] = useState(false);
+  const [count, setCount] = useState(0)
 
   const increment =()=>{
+    alert(`count is ${count}`)
     setCount(count+1);
   }
   return (
@@ -17,37 +15,10 @@ function App() {
       
       <h1>count is {count}</h1>
       <div className="card">
-        <button onClick={increment}>increment</button>
-      </div>
-      <button onClick={()=>setIsModalOpen(true)}>open modal</button>
-      <h1>implementing A</h1>
-      <h1>implementing B</h1>
-      <h1>implementing C</h1>
-      <Modal
-        isOpen={isModalOpen}
-        onRequestClose={() => setIsModalOpen(false)}
-        style={{
-          content: {
-            width: "100px",
-            height:"100px",
-            margin: "auto",
-            padding: "20px",
-            borderRadius: "10px",
-            textAlign: "center",
-          },
-        }}
-      >
-        <h1 style={{ color:'black'}}>count value is{count}</h1>
-        <button
-          style={{
-            backgroundColor: "white",
-            color: "red",
-          }}
-          onClick={() => setIsModalOpen(false)}
-        >
-          Close
+        <button onClick={increment}>
+          increment
         </button>
-      </Modal>
+      </div>
       
     </>
   )
