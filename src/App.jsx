616458c80d79a7b6246f8a5820511aad66a7c31f
PrudhviKleza <prudhvi.macherla@kleza.io>
2025-01-22 11:49:22 +0530

    Revert "Revert "Feature3""
    This reverts commit ff8f3f81d76fb0e45bbe1d38af4a27e553ea5ada.

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Modal from "react-modal";
 
+Modal.setAppElement("#root");
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const increment =()=>{
-    setCount(count+1);
-  }
+  const increment = () => {
+    alert(`count is ${count}`);
+    setCount(count + 1);
+  };
   return (
     <>
-      
       <h1>count is {count}</h1>
       <div className="card">
-        <button onClick={increment}>
-          increment
-        </button>
+        <button onClick={increment}>increment</button>
       </div>
-      
+      <button onClick={()=>setIsModalOpen(true)}>open modal</button>
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={() => setIsModalOpen(false)}
+        style={{
+          content: {
+            width: "300px",
+            margin: "auto",
+            padding: "20px",
+            borderRadius: "10px",
+            textAlign: "center",
+          },
+        }}
+      >
+        <h1 style={{ color:'black'}}>count value is{count}</h1>
+        <button
+          style={{
+            backgroundColor: "#dc3545",
+            color: "red",
+          }}
+          onClick={() => setIsModalOpen(false)}
+        >
+          Close
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
